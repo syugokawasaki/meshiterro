561d17d60a9b5bf152c14c2db44cf5733fe39977
syugokawasaki <n.m.raminas@gmail.com>
2021-07-16 00:39:04 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '791ff656adb7e0f8893273ddc2abcd4847c37f59f577c42e6370d97f70ccb0598efd3a115af2a4ff11c7d64ba68cb269d79a1c7727a513c8407f190d6460bce6'
